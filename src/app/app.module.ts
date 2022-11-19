6a322d5cb4145e63fd45a89db282b158710b8073
FRIFITAmarwan <plusmarwan@gmail.com>
2022-11-20 00:16:52 +0100

    about.component contacts.component -Affichage des données REST avec angular -> done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { ContactsComponent } from './contacts/contacts.component';
+import { AboutComponent } from './about/about.component';
+import {RouterModule, Routes} from "@angular/router";
+import * as path from "path";
+import {HttpClientModule} from "@angular/common/http";
+
+const  appRoutes:Routes=[
+  {path:'about',component:AboutComponent},
+  {path:'contact',component:ContactsComponent},
+  {path:'',redirectTo:'/about',pathMatch:'full'}
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    ContactsComponent
+    ContactsComponent,
+    AboutComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,RouterModule.forRoot(appRoutes),HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
